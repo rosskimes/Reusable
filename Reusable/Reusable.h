6e849317ecd86af03043d0d28b8398693e799ae7
Ross Kimes <ross@rosskimes.net>
2018-01-28 22:16:19 -0600

    Initial Commit

---
+++ Reusable/Reusable.h
@@ -0,0 +1,19 @@
+//
+//  Reusable.h
+//  Reusable
+//
+//  Created by Ross Kimes on 1/28/18.
+//  Copyright © 2018 rosskimes.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reusable.
+FOUNDATION_EXPORT double ReusableVersionNumber;
+
+//! Project version string for Reusable.
+FOUNDATION_EXPORT const unsigned char ReusableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reusable/PublicHeader.h>
+
+
